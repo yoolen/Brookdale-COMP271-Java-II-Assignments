3404bf3d32fd8510755d6a34f7dc15c74ec3645e
yoolen <yoolen@gmail.com>
2014-04-20 15:42:51 -0400

    Added banner; reorganized panels; added Listeners to ToppingsPanel and moved instantiation of JCheckBoxes into constructor; added default states for topping selection.

--- src/pizza/PizzaOrderGUI.java
+++ src/pizza/PizzaOrderGUI.java
@@ -18,6 +18,7 @@ public class PizzaOrderGUI extends JFrame{
 		setLayout(new FlowLayout());
 				
 		// Title Panel
+		add(new JLabel(new ImageIcon(this.getClass().getResource("Banner.jpg"))));
 		
 		// Customer Panel
 		JPanel customerPanel = new JPanel();
@@ -32,12 +33,19 @@ public class PizzaOrderGUI extends JFrame{
 		
 		// Toppings Panel
 		toppingsPanel = new ToppingsPanel();
+		toppingsPanel.setBorder(BorderFactory.createTitledBorder("Toppings ($0.75 each)"));
+		// initialized to synchronize with the default Large pizza size!
 				
-		add(customerPanel);
-		add(sizePanel);
-		add(toppingsPanel);
+		// Choice Panel
+		JPanel choicePanel = new JPanel();
+		choicePanel.add(sizePanel);
+		choicePanel.add(toppingsPanel);
 		
-	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// Build the Final GUI
+		add(customerPanel);
+		add(choicePanel);		
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    setSize(580,600); // Need to set the size before centering
 	    setLocationRelativeTo(null); // Center the frame
 		setResizable(false); // prevent the user from resizing the window
@@ -54,6 +62,7 @@ public class PizzaOrderGUI extends JFrame{
 		private JRadioButton rdbtnXLarge = new JRadioButton("X-Large");
 			
 		public SizePanel(){
+			setLayout(new GridLayout(5,1));
 			rdbtnSmall = new JRadioButton("Small");
 			rdbtnSmall.setMnemonic(KeyEvent.VK_S);
 			rdbtnSmall.addActionListener(this);
@@ -80,6 +89,8 @@ public class PizzaOrderGUI extends JFrame{
 			add(rdbtnMedium);
 			add(rdbtnLarge);
 			add(rdbtnXLarge);
+			rdbtnLarge.setSelected(true); // Initialize to large pie!
+			myPizza = new LargePizza(); // Instantiate a large pie!			
 		}
 		
 		@Override
@@ -88,42 +99,92 @@ public class PizzaOrderGUI extends JFrame{
 		switch(ae.getActionCommand()){
 			case "Small":
 				myPizza = new SmallPizza();
+				toppingsPanel.setBorder(BorderFactory.createTitledBorder("Toppings ($0.25 each)"));
 				break;
 			case "Medium":
 				myPizza = new MediumPizza();
+				toppingsPanel.setBorder(BorderFactory.createTitledBorder("Toppings ($0.50 each)"));
 				break;
 			case "Large":
 				myPizza = new LargePizza();
+				toppingsPanel.setBorder(BorderFactory.createTitledBorder("Toppings ($0.75 each)"));
 				break;
-			case "XLarge":
+			case "X-Large":
 				myPizza = new XLargePizza();
+				toppingsPanel.setBorder(BorderFactory.createTitledBorder("Toppings ($1.00 each)"));
 				break;
 			default:
 		}
+		System.out.println(myPizza);
 		((ToppingsPanel)toppingsPanel).reset();
 		// Changing pizza sizes resets all selections.
 	}
 }
 	
 	private class ToppingsPanel extends JPanel implements ItemListener{
-		private JCheckBox anchovies = new JCheckBox("Anchovies");
-		private JCheckBox bacon = new JCheckBox("Bacon");
-		private JCheckBox ham = new JCheckBox("Ham");
-		private JCheckBox pepperoni = new JCheckBox("Pepperoni");
-		private JCheckBox sausage = new JCheckBox("Sausage");
-		private JCheckBox bellPeppers = new JCheckBox("Bell Peppers");
-		private JCheckBox onions = new JCheckBox("Onions");
-		private JCheckBox eggplant = new JCheckBox("Eggplant");
-		private JCheckBox garlic = new JCheckBox("Garlic");
-		private JCheckBox mushrooms = new JCheckBox("Mushrooms");
-		private JCheckBox spinach = new JCheckBox("Spinach");
-		private JCheckBox olives = new JCheckBox("Olives");
-		private JCheckBox pineapple = new JCheckBox("Pineapple");
-		private JCheckBox xCheese = new JCheckBox("Extra Cheese");
+		private JCheckBox anchovies;
+		private JCheckBox bacon;
+		private JCheckBox ham;
+		private JCheckBox pepperoni;
+		private JCheckBox sausage;
+		private JCheckBox bellPeppers;
+		private JCheckBox onions;
+		private JCheckBox eggplant;
+		private JCheckBox garlic;
+		private JCheckBox mushrooms;
+		private JCheckBox spinach;
+		private JCheckBox olives;
+		private JCheckBox pineapple;
+		private JCheckBox xCheese;
 		
 		public ToppingsPanel(){
-			JPanel toppingPanel = new JPanel(new GridLayout(1,3));
+			JPanel toppingsPanel = new JPanel();
 			
+			// Initialize checkboxes and attach listeners
+			anchovies = new JCheckBox("Anchovies");
+			anchovies.setName("Anchovies");
+			anchovies.addItemListener(this);
+			bacon = new JCheckBox("Bacon");
+			bacon.setName("Bacon");
+			bacon.addItemListener(this);
+			ham = new JCheckBox("Ham");
+			ham.setName("Ham");
+			ham.addItemListener(this);
+			pepperoni = new JCheckBox("Pepperoni");
+			pepperoni.setName("Pepperoni");
+			pepperoni.addItemListener(this);
+			sausage = new JCheckBox("Sausage");
+			sausage.setName("Sausage");
+			sausage.addItemListener(this);
+			bellPeppers = new JCheckBox("Bell Peppers");
+			bellPeppers.setName("Bell Peppers");
+			bellPeppers.addItemListener(this);
+			onions = new JCheckBox("Onions");
+			onions.setName("Onions");
+			onions.addItemListener(this);
+			eggplant = new JCheckBox("Eggplant");
+			eggplant.setName("Eggplant");
+			eggplant.addItemListener(this);
+			garlic = new JCheckBox("Garlic");
+			garlic.setName("Garlic");
+			garlic.addItemListener(this);
+			mushrooms = new JCheckBox("Mushrooms");
+			mushrooms.setName("Mushrooms");
+			mushrooms.addItemListener(this);
+			spinach = new JCheckBox("Spinach");
+			spinach.setName("Spinach");
+			spinach.addItemListener(this);
+			olives = new JCheckBox("Olives");
+			olives.setName("Olives");
+			olives.addItemListener(this);
+			pineapple = new JCheckBox("Pineapple");
+			pineapple.setName("Pineapple");
+			pineapple.addItemListener(this);
+			xCheese = new JCheckBox("Extra Cheese");
+			xCheese.setName("Extra Cheese");
+			xCheese.addItemListener(this);
+			
+			// Meats
 			JPanel meat = new JPanel(new GridLayout(5,1));
 			meat.setBorder(BorderFactory.createTitledBorder("Meat"));
 			meat.add(anchovies);
@@ -132,30 +193,27 @@ public class PizzaOrderGUI extends JFrame{
 			meat.add(pepperoni);
 			meat.add(sausage);
 			
-			JPanel veg = new JPanel();
+			// Vegetables
+			JPanel veg = new JPanel(new GridLayout(5,2));
 			veg.setBorder(BorderFactory.createTitledBorder("Vegetables"));
-			
-			JPanel veg1 = new JPanel(new GridLayout(5,1));
-			veg1.add(bellPeppers);
-			veg1.add(spinach);
-			veg1.add(onions);
-			veg1.add(eggplant);
-			veg1.add(olives);
-			
-			JPanel veg2 = new JPanel(new GridLayout(5,1));
-			veg2.add(pineapple);
-			veg2.add(garlic);
-			veg2.add(mushrooms);
-			veg2.add(xCheese);
+			veg.add(bellPeppers);
+			veg.add(spinach);
+			veg.add(onions);
+			veg.add(eggplant);
+			veg.add(olives);
+			veg.add(pineapple);
+			veg.add(garlic);
+			veg.add(mushrooms);
+			veg.add(xCheese);
 
-			veg.add(veg1);
-			veg.add(veg2);
-			
-			add(meat);
-			add(veg);
+			// Add both subpanels into the toppings panel
+			toppingsPanel.add(meat);
+			toppingsPanel.add(veg);
+			add(toppingsPanel);
 		}
 		
-		public void reset(){
+ 		public void reset(){
+		// Reset all selections
 			anchovies.setSelected(false);
 			bacon.setSelected(false);
 			ham.setSelected(false);
@@ -166,20 +224,27 @@ public class PizzaOrderGUI extends JFrame{
 			eggplant.setSelected(false);
 			garlic.setSelected(false);
 			mushrooms.setSelected(false);
+			spinach.setSelected(false);
 			olives.setSelected(false);
 			pineapple.setSelected(false);
 			xCheese.setSelected(false);
 		}
 
 		@Override
-		public void itemStateChanged(ItemEvent arg0) {
-			
-			
+		public void itemStateChanged(ItemEvent ie) {
+		// Each time a topping is added or removed from the pizza
+		// we use the built in methods from Pizza to change the list
+		// of toppings and change the price!
+			if(ie.getStateChange()==1){
+				myPizza.addTopping(((Component)ie.getSource()).getName());
+			}
+			else{//(ie.getStateChange()==0){
+				myPizza.removeTopping(((Component)ie.getSource()).getName());
+			}
 		}
-		
 	}
 	
-	private double calculatePrice(){
+	private double checkout(){
 	// Uses Pizza's getPrice method and checks if user wants delivery
 		double price = 0;
 		return price;
